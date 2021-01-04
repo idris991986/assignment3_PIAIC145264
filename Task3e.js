cb6fd73c1a0008ad5c2fda327226de1b7a0caa3f
Muhammad Idrees <m.idris991986@gmail.com>
2021-01-04 09:09:57 -0800

    Completed Task 3a of Javascript Assignment 3e

---
+++ Task3e.js
@@ -0,0 +1,11 @@
+var colors = ["Blue","Green","Yellow"];
+document.write("Old Elements are : "+colors);
+colors.unshift("Black","Purple");
+document.write("<br>");document.write("<br>");
+document.write("New Elements are : "+colors);
+colors.shift();
+document.write("<br>");document.write("<br>");
+document.write("Updated Elements are : "+colors);
+colors.pop();
+document.write("<br>");document.write("<br>");
+document.write("New Updated Elements are : "+colors);
