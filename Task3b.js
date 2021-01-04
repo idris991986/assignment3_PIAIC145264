f12e96941174794e827be47be61b606cedf63cf4
Muhammad Idrees <m.idris991986@gmail.com>
2021-01-04 09:00:27 -0800

    Completed Task 3a of Javascript Assignment 3b

---
+++ Task3b.js
@@ -0,0 +1,6 @@
+var colors = ["Blue","Green","Yellow"];
+document.write("Old Elements are : "+colors);
+var newcolor = prompt("What color you want to add at the beginning?");
+colors.push(newcolor);
+document.write("<br>");document.write("<br>");
+document.write("New Elements are : "+colors);
