bee897d6c8a8a9f496376b8bdf88b6c7477f93d3
Muhammad Idrees <m.idris991986@gmail.com>
2021-01-07 00:58:29 -0800

    Completed Task 4 of Javascript Assignment 3

---
+++ Task4.js
@@ -0,0 +1,10 @@
+var cities = ["Karachi", "Lahore", "Islamabad", "Quetta", "Peshawar"];
+document.write("Cities List :");
+document.write("<br>");
+document.write(cities);
+document.write("<br>");
+document.write("<br>");
+var selectedCities = cities.slice(2,4);
+document.write("Selected Cities List :");
+document.write("<br>");
+document.write(selectedCities);
